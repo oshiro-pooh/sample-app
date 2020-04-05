757cbdef444c4e49f6a5a4b612b934b935f6c808
oshiro-pooh <oshirofindme@gmail.com>
2020-04-06 01:43:28 +0900

    レクチャー68まで実装

--- src/main.js
+++ src/main.js
@@ -3,8 +3,21 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import vuetify from './plugins/vuetify';
+import firebase from 'firebase';
 
 Vue.config.productionTip = false
+
+const config = {
+  apiKey: "AIzaSyDZ91hjbhnf7aNox9EMpenDkvRkdJ_ducc",
+  authDomain: "my-address-p.firebaseapp.com",
+  databaseURL: "https://my-address-p.firebaseio.com",
+  projectId: "my-address-p",
+  storageBucket: "my-address-p.appspot.com",
+  messagingSenderId: "214320304184",
+  appId: "1:214320304184:web:ef946d922487b9613456cd",
+  measurementId: "G-Q3S1EFVS83"
+};
+firebase.initializeApp(config);
 
 new Vue({
   router,
